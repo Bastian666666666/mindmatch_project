c7537236d7d877cd294fa7f42575069666815552
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-24 16:29:16 -0400

    1

--- src/app/services/routeguard.guard.ts
+++ src/app/services/routeguard.guard.ts
@@ -1,10 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, ActivatedRouteSnapshot } from '@angular/router';
 
-export const routeguardGuard: CanActivateFn = (route, state) => {
+export const routeguardGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state) => {
   const username = sessionStorage.getItem('username');
+  const adminRoutes = ['adminhome', 'admin-agregar', 'admin-modificar'];
+  const isAccessingAdminRoute = adminRoutes.some(adminRoute => state.url.includes(adminRoute));
+
   if (username) {
-    // Si el username existe en sessionStorage, permite el acceso
-    return true;
+    // Si el username existe y no está intentando acceder a una ruta de administrador, permite el acceso
+    if (!isAccessingAdminRoute) return true;
+    // Si está intentando acceder a una ruta de administrador, verifica si el username es "RoyB"
+    return username === "RoyB";
   } else {
     // Si no hay username, no permite el acceso
     return false;
