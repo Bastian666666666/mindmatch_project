06ed387b3780637d3f59fb7eec5dc00ecb4da425
Bastián Rabelo <ba.rabelo@duocuc.cl>
2024-06-24 02:46:22 -0400

    routeguard creada guard y agregada a pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { routeguardGuard } from './services/routeguard.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
+    canActivate: [routeguardGuard] // Agrega tu guard aquí
   },
   
   {
@@ -18,39 +20,48 @@ const routes: Routes = [
   },
   {
     path: 'tutorial',
-    loadChildren: () => import('./pages/tutorial/tutorial.module').then( m => m.TutorialPageModule)
+    loadChildren: () => import('./pages/tutorial/tutorial.module').then( m => m.TutorialPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'ev',
-    loadChildren: () => import('./pages/ev/ev.module').then( m => m.EvPageModule)
+    loadChildren: () => import('./pages/ev/ev.module').then( m => m.EvPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'resultado',
-    loadChildren: () => import('./pages/resultado/resultado.module').then( m => m.ResultadoPageModule)
+    loadChildren: () => import('./pages/resultado/resultado.module').then( m => m.ResultadoPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'not-found',
-    loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule)
+    loadChildren: () => import('./pages/not-found/not-found.module').then( m => m.NotFoundPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'adminhome',
-    loadChildren: () => import('./pages/adminhome/adminhome.module').then( m => m.AdminhomePageModule)
+    loadChildren: () => import('./pages/adminhome/adminhome.module').then( m => m.AdminhomePageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'admin-agregar',
-    loadChildren: () => import('./pages/admin-agregar/admin-agregar.module').then( m => m.AdminAgregarPageModule)
+    loadChildren: () => import('./pages/admin-agregar/admin-agregar.module').then( m => m.AdminAgregarPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'admin-modificar',
-    loadChildren: () => import('./pages/admin-modificar/admin-modificar.module').then( m => m.AdminModificarPageModule)
+    loadChildren: () => import('./pages/admin-modificar/admin-modificar.module').then( m => m.AdminModificarPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'micuenta',
-    loadChildren: () => import('./pages/micuenta/micuenta.module').then( m => m.MicuentaPageModule)
+    loadChildren: () => import('./pages/micuenta/micuenta.module').then( m => m.MicuentaPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: 'camara',
-    loadChildren: () => import('./pages/camara/camara.module').then( m => m.CamaraPageModule)
+    loadChildren: () => import('./pages/camara/camara.module').then( m => m.CamaraPageModule),
+    canActivate: [routeguardGuard] //
   },
   {
     path: '',
